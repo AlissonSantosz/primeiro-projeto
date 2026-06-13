8ea5cce8db3b9a9312c86365be697a61a8761c41
Alisson Santosz <alissonsantos200113@gmail.com>
2026-06-12 21:25:47 -0300

    Aprendendo variáveis e imprimir na tela

---
+++ programa01.py
@@ -0,0 +1,10 @@
+nome = "alisson"
+ultimo_nome = "santos"
+idade = 29
+altura = 1.67
+maior_de_idade = True
+
+print ("Olá ", nome, ultimo_nome, "voce tem ", idade, "anos, sua altura é?",altura, "voce é maior de idade", maior_de_idade, "!")
+
+print(f"Olá {nome} {ultimo_nome} voce tem {idade} anos, sua altura é {altura}, voce é maior de idade? {maior_de_idade}!")
+
