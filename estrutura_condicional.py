e7a3a213757487f00a92e219ffc85183948aef10
Alisson Santosz <alissonsantos200113@gmail.com>
2026-06-16 21:29:20 -0300

    aprendendo condicional if

---
+++ estrutura_condicional.py
@@ -0,0 +1,18 @@
+# Casar ou Comprar uma Bicicleta ?
+
+print(" Casar ou comprar uma Bicicleta ?")
+
+resposta = input("Você está gordo ? s/n: ")
+
+if resposta == "s":
+    quer_emagrecer = input("Você quer emagrecer ?")
+    if quer_emagrecer == "s":
+        print("compre uma bicicleta!")
+    else:
+        print("Então case!")
+else:
+    quer_engordar = input("Você quer engordar ? s/n: ")
+    if quer_engordar == "s":
+        print("Então Case!")
+    else:
+        print("Compre uma bicicleta!")
