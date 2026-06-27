46ff668edf6a28de00fd38129acefcebee86e1c0
Alisson Santosz <alissonsantos200113@gmail.com>
2026-06-26 21:29:42 -0300

    apredendo vetores

---
+++ atividades/vetores.py
@@ -0,0 +1,20 @@
+aluno = ["Davi", "Lucca", "Alisson", "Ana"]
+print(aluno[0])
+print(aluno[1])
+print(aluno[2])
+print(aluno[3])
+
+numeros = (1, 2, 3 ,4, 5)
+print(numeros[0])
+print(numeros[1])
+print(numeros[2])
+print(numeros[3])
+print(numeros[4])
+
+notas = (8.5, 9.5, 1.2, 3.6, 4.9)
+print(notas[0])
+print(notas[1])
+print(notas[2])
+print(notas[3])
+print(notas[4])
+
